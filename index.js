280f8175ac426a82fc9e9e14b7490a03674a9134
Konsta <konsta.karjalainen@tuni.fi>
2023-06-03 22:40:03 +0300

    static express

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan('tiny'))
+app.use(cors())
 
 morgan.token('postToken', (req, res) => JSON.stringify(req.body))
 
@@ -88,7 +91,7 @@ app.post('/api/persons', (req,res) => {
 	res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
